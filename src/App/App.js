fef8c0eeb1f0e2e91b6d82bbc781cf9ec60e120b
HXgrowns <804156017@qq.com>
2020-09-07 20:44:00 +0800

    [huxiao]: finish dynamic loading data function

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,25 @@ import './App.scss';
 import '../style/resetting.scss'
 import "../style/reboot.scss";
 class App extends Component {
+  state = {
+    students:[],
+  }
+  componentDidMount() {
+    fetch("http://127.0.0.1:8080/student/list", {
+        method: 'GET',
+    }).then((response) => {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(new Error(response.status + response.statusText));
+    }).then((data) => {
+        this.setState({
+          students: data,
+        });
+    }).catch((err) => {
+        console.error(err);
+    });
+}
   datas = [{
     id: 1,
     name: '沈乐起'
@@ -71,7 +90,7 @@ class App extends Component {
         <main className="student-list">
           <h3>学员列表</h3>
           <section>
-            {this.datas.map((data, index) =>
+            {this.state.students.map((data, index) =>
               <input key={index} type="text" value={data.id + '.' + data.name} />
             )}
             <input className="add-student" type="text" placeholder="+添加学员" />
