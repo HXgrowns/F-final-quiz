3726a458fcb052b0e26b8eee92d162bfe41b2a55
HXgrowns <804156017@qq.com>
2020-09-07 16:35:43 +0800

    [huxiao]: add nav

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
 import '../style/resetting.scss'
+import "../style/reboot.scss";
 class App extends Component {
-  
+
   render() {
     return (
       <main data-testid="app" className="App">
-        <nav className = "nav">
-          nav
+        <nav className="nav">
+          <h2>分组列表</h2>
+          <button>分组学员</button>
         </nav>
-        <section className = "student-list">
-        name
+        <section className="student-list">
+          name
         </section>
       </main>
     );
