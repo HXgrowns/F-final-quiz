9b3c193ecd03ff5afbd0e55e12de9b53aab88f66
HXgrowns <804156017@qq.com>
2020-09-07 18:15:43 +0800

    [huxiao]: add main

--- src/App/App.js
+++ src/App/App.js
@@ -3,18 +3,81 @@ import './App.scss';
 import '../style/resetting.scss'
 import "../style/reboot.scss";
 class App extends Component {
+  datas = [{
+    id: 1,
+    name: '沈乐起'
+  },
+  {
+    id: 2,
+    name: '徐慧慧',
+  },
+  {
+    id: 1,
+    name: '沈乐起'
+  },
+  {
+    id: 2,
+    name: '徐慧慧',
+  },
+  {
+    id: 1,
+    name: '沈乐起'
+  },
+  {
+    id: 2,
+    name: '徐慧慧',
+  },
+  {
+    id: 1,
+    name: '沈乐起'
+  },
+  {
+    id: 2,
+    name: '徐慧慧',
+  },
+  {
+    id: 1,
+    name: '沈乐起'
+  },
+  {
+    id: 2,
+    name: '徐慧慧',
+  },
+  {
+    id: 1,
+    name: '沈乐起'
+  },
+  {
+    id: 2,
+    name: '徐慧慧',
+  },
+  {
+    id: 1,
+    name: '沈乐起'
+  },
+  {
+    id: 2,
+    name: '徐慧慧',
+  }
+  ]
 
   render() {
     return (
-      <main data-testid="app" className="App">
+      <div data-testid="app" className="App">
         <nav className="nav">
           <h2>分组列表</h2>
           <button>分组学员</button>
         </nav>
-        <section className="student-list">
-          name
-        </section>
-      </main>
+        <main className="student-list">
+          <h3>学员列表</h3>
+          <section>
+            {this.datas.map((data, index) =>
+              <input key={index} type="text" value={data.id + '.' + data.name} disabled />
+            )}
+            <input className ="add-student" type = "text" placeholder = "+添加学员"/>
+          </section>
+        </main>
+      </div>
     );
   }
 }
